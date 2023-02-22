0c0f424ad6232d53814077dbb30a95d5fba14a29
Srija cheruku <sxc54870@ucmo.edu>
2023-02-21 18:27:13 -0800

    change

--- hello1.py
+++ hello1.py
@@ -1,1 +1,11 @@
-print("HELLO WORLD") #my demo
+def hello():
+ f = open("text1.txt", "r")
+ print(f.read())
+   
+hello()
+
+def hello():
+  f = open("text2.text","r")
+  print(f.read())
+
+ hello() 
